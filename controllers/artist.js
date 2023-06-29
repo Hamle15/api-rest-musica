35425bd10dd9c27a0b3c19335946999e6f19faf0
Hamle15 <80331657+Hamle15@users.noreply.github.com>
2023-06-29 11:26:14 -0500

    Method: one save

--- controllers/artist.js
+++ controllers/artist.js
@@ -1,3 +1,6 @@
+// Imports
+const Artist = require("../models/Artist");
+
 // Test accion
 const prueba = (req, res) => {
   return res.status(200).send({
@@ -6,7 +9,87 @@ const prueba = (req, res) => {
   });
 };
 
+const save = (req, res) => {
+  // Take the data of th body
+  let params = req.body;
+
+  if (!params.name) {
+    return res.status(400).send({
+      status: "error",
+      message: "The name is require",
+    });
+  }
+
+  // Check if duplicate Artist
+  Artist.find({ name: params.name })
+    .then((artistD) => {
+      if (artistD && artistD.length >= 1) {
+        return res.status(409).send({
+          status: "error",
+          message: "The artist alredy exist",
+        });
+      }
+      // Crate the obj of the body
+      let artist = new Artist(params);
+
+      // Saved
+      artist
+        .save()
+        .then((artistStored) => {
+          if (!artistStored) {
+            return res.status(500).send({
+              status: "error",
+              message: "The artist could not be saved",
+            });
+          }
+          return res.status(200).send({
+            status: "succes",
+            artist: artistStored,
+          });
+        })
+        .catch((error) => {
+          return res.status(500).send({
+            status: "error",
+            message: "The artist could not be saved",
+          });
+        });
+    })
+    .catch((error) => {
+      return res.status(409).send({
+        status: "error",
+        message: "The artist alredy exist",
+      });
+    });
+};
+
+const one = (req, res) => {
+  // Take the params from the url
+  const artistId = req.params.id;
+  // Find
+  Artist.findById(artistId)
+    .then((artist) => {
+      if (!artist) {
+        return res.status(404).send({
+          status: "error",
+          message: "The user do not exist",
+        });
+      }
+      return res.status(200).send({
+        status: "succes",
+        artist: artist,
+      });
+    })
+    .catch((error) => {
+      return res.status(404).send({
+        status: "error",
+        message: "The user do not exist",
+      });
+    });
+};
+
 // Export accions
 module.exports = {
   prueba,
+  save,
+  one,
 };
