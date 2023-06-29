40d50fe56f0b1faac42352da7b5dee02c0cf0630
Hamle15 <80331657+Hamle15@users.noreply.github.com>
2023-06-29 11:25:55 -0500

    Artist model, and routers

--- routers/artist.js
+++ routers/artist.js
@@ -1,14 +1,16 @@
 // Import dependencies
 const express = require("express");
+const check = require("../middlewares/auth");
 
 // Carge router
 const router = express.Router();
 
 // Import controller
-const ArtisrController = require("../controllers/artist");
+const ArtistController = require("../controllers/artist");
 
 // Define routes
-router.get("/test-artist", ArtisrController.prueba);
-
+router.get("/test-artist", ArtistController.prueba);
+router.post("/save", check.auth, ArtistController.save);
+router.get("/one/:id", check.auth, ArtistController.one);
 // Export routes
 module.exports = router;
