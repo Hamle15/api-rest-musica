dd1ea47f12c386f5e33022bbd3985f7b81c1edef
Hamle15 <80331657+Hamle15@users.noreply.github.com>
2023-07-04 06:46:36 -0500

    routes for album: savem one and list

--- controllers/album.js
+++ controllers/album.js
@@ -1,3 +1,6 @@
+// Imports
+const Album = require("../models/Album");
+
 // Test accion
 const prueba = (req, res) => {
   return res.status(200).send({
@@ -6,7 +9,100 @@ const prueba = (req, res) => {
   });
 };
 
+const save = (req, res) => {
+  // Take data from the body
+  let params = req.body;
+
+  // Validate Data
+
+  if (params.type !== "Single" && params.type !== "Album") {
+    return res.status(400).send({
+      status: "error",
+      message: "The field have to be a type Single or Album",
+    });
+  }
+
+  // Create Obj
+  let album = new Album(params);
+
+  // Save the obj
+  album
+    .save()
+    .then((albumStored) => {
+      if (!albumStored) {
+        return res.status(200).send({
+          status: "succes",
+          message: "path save album",
+          album,
+        });
+      }
+      return res.status(200).send({
+        status: "succes",
+        albumStored,
+      });
+    })
+    .catch((error) => {
+      return res.status(500).send({
+        status: "error",
+        message: "Could not save the album",
+      });
+    });
+};
+
+const one = (req, res) => {
+  // Take the id from the album
+  const albumId = req.params.id;
+
+  // Find album and take the artist info
+  Album.findOne({ _id: albumId })
+    .populate({ path: "artist", select: "-__v" })
+
+    .then((albumSaved) => {
+      if (!albumSaved) {
+        return res.status(404).send({
+          status: "error",
+          message: "The album do not exist",
+        });
+      }
+      // Return result
+      return res.status(200).send({
+        status: "succes",
+        album: albumSaved,
+      });
+    })
+    .catch((error) => {
+      return res.status(404).send({
+        status: "error",
+        message: "The album do not exist",
+      });
+    });
+};
+
+const list = (req, res) => {
+  // Get the id from the artist from the url
+  const artistId = req.params.artistId;
+  // Get all the albums from a artist
+  Album.find({ artist: artistId })
+    .populate("artist")
+    .then((albums) => {
+      if (!albums) {
+        return res.status(404).send({
+          status: "error",
+          message: "The artist do not exist",
+        });
+      }
+      // Return result
+      return res.status(200).send({
+        status: "succes",
+        albums,
+      });
+    });
+};
+
 // Export accions
 module.exports = {
   prueba,
+  save,
+  one,
+  list,
 };
--- routers/album.js
+++ routers/album.js
@@ -1,5 +1,7 @@
 // Import dependencies
 const express = require("express");
+const check = require("../middlewares/auth");
+const { authRole } = require("../middlewares/authRole");
 
 // Carge router
 const router = express.Router();
@@ -9,6 +11,24 @@ const AlbumController = require("../controllers/album");
 
 // Define routes
 router.get("/test-album", AlbumController.prueba);
+router.post(
+  "/save",
+  check.auth,
+  authRole(["role_admin"]),
+  AlbumController.save
+);
+router.get(
+  "/one/:id",
+  check.auth,
+  authRole(["role_admin"]),
+  AlbumController.one
+);
+router.get(
+  "/list/:artistId",
+  check.auth,
+  authRole(["role_admin"]),
+  AlbumController.list
+);
 
 // Export routes
 module.exports = router;
