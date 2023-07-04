e02866eace823aa3fd9d7f0d10924b0a88ff440d
Hamle15 <80331657+Hamle15@users.noreply.github.com>
2023-07-04 06:48:52 -0500

    Album and Song Schema

---
+++ models/Album.js
@@ -0,0 +1,33 @@
+const { Schema, model } = require("mongoose");
+
+const AlbumSchema = Schema({
+  artist: {
+    type: Schema.ObjectId,
+    ref: "Artist",
+  },
+  type: {
+    type: String,
+    enum: ["Single", "Album"],
+    required: true,
+  },
+  title: {
+    type: String,
+    required: true,
+  },
+
+  description: String,
+  year: {
+    type: Number,
+    required: true,
+  },
+  image: {
+    type: String,
+    default: "default.png",
+  },
+  created_at: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+module.exports = model("Album", AlbumSchema, "albums");
